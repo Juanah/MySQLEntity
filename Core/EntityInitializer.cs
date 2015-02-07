943baf41b6b68b401f68f03fe0e5b94fee535b3b
jonas ahlf <jonas@juanah.de>
2015-02-07 12:32:19 +0100

    Linux version sadly cannot use Bootstrapper so we use the Container itself instead

--- Core/EntityInitializer.cs
+++ Core/EntityInitializer.cs
@@ -3,6 +3,7 @@ using Infrastructure.Core;
 using System.Collections.Generic;
 using Infrastructure;
 using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
 
 
 namespace Core
@@ -12,18 +13,21 @@ namespace Core
 	/// </summary>
 	public class EntityInitializer
 	{
+
+		IUnityContainer container;
+
 		public EntityInitializer (IDBConnectionInfo _connectionInfo)
 		{
 			this._connectionInfo = _connectionInfo;
+			container = new UnityContainer ();
 		}
 
 		public Context GetContext(List<IEntity> entities)
 		{
 			Init ();
-
 			Context context = new Context (
-				ServiceLocator.Current.GetInstance<IClassParser> (),
-				ServiceLocator.Current.GetInstance<ISqlQueryProcessor> (),
+				this.container.Resolve<IClassParser>(),
+				this.container.Resolve<ISqlQueryProcessor>(),
 				_connectionInfo
 				);
 			context.Entities = entities;
@@ -32,11 +36,20 @@ namespace Core
 
 		private void Init()
 		{
+
+			IConnection _connection = new Connection (_connectionInfo);
+
+			this.container.RegisterInstance<IDBConnectionInfo>(_connectionInfo, new ContainerControlledLifetimeManager ());
+			this.container.RegisterInstance<IConnection> (_connection, new ContainerControlledLifetimeManager ());
+			this.container.RegisterType<ISqlQueryProcessor,SqlQueryProcessor> (new ContainerControlledLifetimeManager ());
+			this.container.RegisterType<IClassParser,BaseParser>(new ContainerControlledLifetimeManager ());
+
+
 			if (_connectionInfo == null) {
 				throw new ArgumentNullException ("IDBConnectionInfo");
 			}
-			CoreBootstrapper coreBootstrapper = new CoreBootstrapper (_connectionInfo);
-			coreBootstrapper.Run ();
+			//CoreBootstrapper coreBootstrapper = new CoreBootstrapper (_connectionInfo);
+			//coreBootstrapper.Run ();
 		
 		}
 
